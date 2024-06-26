77d67b43e8c3e95f70fa8900b5acd6f8a6040eb8
AlexandraQWZ <165285746+AlexandraQWZ@users.noreply.github.com>
2024-06-26 18:27:17 +0700

    Delete lib directory

--- lib/models/cart.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:travel_apps/models/travel.dart';
-
-class Cart with ChangeNotifier {
-  Travel travel;
-  int jumlah = 0;
-
-  Cart({required this.travel, this.jumlah = 0});
-}
